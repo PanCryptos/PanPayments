c0c313e213ac5b16580d26ac967d2d55a1c21b5f
korede <118663191+koredecodes@users.noreply.github.com>
2023-02-01 20:17:51 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+$api = "test@pancryptos.com"; # Aqui debes poner tu API KEY cual aparece en https://pancryptos.com/login/
+$shop_name = "TIENDA EJEMPLO"; # Aqui pones el nombre de tu tienda
+$Pago_Cantidad = 10; # Aqui pones el monto que vas a cobrar
+$Return = "http://localhost/PAGO_CON_PANPAYMENTS/pago.php"; # Aqui pones la ubicacion del archivo pago.php
+
+$url = "https://pancryptos.com/api/PanPayments/pay.php?price=" . $Pago_Cantidad . "&shop=" . $shop_name . "&api=" . $api . "&return=" . $Return;
+header('Location: ' . $url);
+?>
