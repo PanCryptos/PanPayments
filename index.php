2de45f077c541c18bd9017faa896e6bc5fb32a70
korede <118663191+koredecodes@users.noreply.github.com>
2023-02-02 18:59:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,5 +5,9 @@ $Pago_Cantidad = 10; # Aqui pones el monto que vas a cobrar
 $Return = "http://localhost/PAGO_CON_PANPAYMENTS/pago.php"; # Aqui pones la ubicacion del archivo pago.php
 
 $url = "https://pancryptos.com/api/PanPayments/pay.php?price=" . $Pago_Cantidad . "&shop=" . $shop_name . "&api=" . $api . "&return=" . $Return;
-header('Location: ' . $url);
+
+$code = file_get_contents($url);
+echo  "Tu link de pago es : " . $code;
+header('Location: ' . $code);
+
 ?>
